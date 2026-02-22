5e20777e1eb313be07e1f5498252f1d3dcdb259b
PiS <giertychdebil@gmail.com>
2026-02-22 18:43:35 +0100

    dodanie kolejengo easteregga
    batyr

--- script.js
+++ script.js
@@ -25,4 +25,41 @@ function closeEasterEgg() {
     video.pause();
     video.src = ""; 
     overlay.style.display = 'none';
+
+}
+
+// --- TAJNA SEKWENCJA ---
+
+const secretSequence = "batyr"; // tu ustawiasz hasło
+let typed = "";
+
+document.addEventListener("keydown", function(e) {
+    typed += e.key.toLowerCase();
+
+    // przycinamy bufor
+    if (typed.length > secretSequence.length) {
+        typed = typed.slice(-secretSequence.length);
+    }
+
+    if (typed === secretSequence) {
+        openEasterEgg();
+    }
+});
+
+function openEasterEgg() {
+    const overlay = document.getElementById("video-overlay");
+    const video = document.getElementById("easter-video-player");
+
+    video.src = "batyr.mp4"; // TU wpisz ścieżkę do filmu
+    overlay.style.display = "flex";
+    video.play();
+}
+
+function closeEasterEgg() {
+    const overlay = document.getElementById("video-overlay");
+    const video = document.getElementById("easter-video-player");
+
+    video.pause();
+    video.currentTime = 0;
+    overlay.style.display = "none";
 }
